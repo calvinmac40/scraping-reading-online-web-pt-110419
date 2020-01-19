4c08ea156c6e6a43be5231856449ca84271910d4
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-19 00:54:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,23 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = Nokogiri::HTML(open("https://flatironschool.com/our-courses/"))
+# titles = html.css("h3.title-3CyKCM").map do |courses|
+#   puts courses.text
+# end
+
+
+
+# html.css(".headline-26OIBN").text.strip
+
+puts description = html.css("#text") #.collect do |descr| 
+#   puts descr.text
+# end
+ 
+#   puts description.each.with_index{|i,v|
+# "#{i}" "#{v}"}
+# #binding.pry
+
+#puts decription_of_course = html.css("div.container-1cfI6E").first.text
+#binding.pry
